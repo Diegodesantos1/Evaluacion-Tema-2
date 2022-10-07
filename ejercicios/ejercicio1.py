2b89fabdca6772260601ceba836530a4b8ea933d
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-07 15:49:15 +0200

    ya funciona correctamente los métodos de crear y calificar

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -5,18 +5,19 @@ class Alumno:
         self.nota = nota
     def __str__(self):
         return f'El alumno se llama: {self.nombre} {self.apellido} y ha sacado un {self.nota}'
-    def crear_alumno(self):
+    def crear_alumno():
+        global nota
         nombre = str(input('Nombre: '))
         apellido = str(input('Apellido: '))
         nota = int(input('Nota: '))
         print("Se ha creado el alumno con éxito")
         print(Alumno(nombre, apellido, nota))
-    def calificacion(self):
-        if self.nota >= 5:
-            print('Aprobado')
+    def calificacion():
+        if nota >= 5:
+            print('El alumno ha aprobado')
         else:
-            print('Suspenso')
+            print('El alumno ha suspendido')
 
-alumno = Alumno('Diego', 'de Santos', 7)
-alumno.calificacion()
+Alumno.crear_alumno()
+Alumno.calificacion()
 
