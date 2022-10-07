799163e1afd633ae3a5cfb5d4ef54a2506344b3c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-07 16:06:36 +0200

    da error y no comprueba todos con los pass, voy a juntarlo en una sola funcion

--- ejercicios/ejercicio4.py
+++ ejercicios/ejercicio4.py
@@ -5,6 +5,7 @@ class excepciones():
             print(numero)
         except ZeroDivisionError:
             print("Error, no se puede dividir por 0")
+        pass
     def comprobar2():
         lista = [4, 7, 30, 23, 5]
         lista[10]
@@ -12,6 +13,7 @@ class excepciones():
             print(lista[10])
         except IndexError:
             print("Error, no existe elemento con ese índice")
+        pass
     def comprobar3():
         paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" }
         paises["alemania"]
@@ -19,11 +21,16 @@ class excepciones():
             print(paises["alemania"])
         except KeyError:
             print("Error, no existe ese elemento en el diccionario")
+        pass
     def comprobar4():
         resultado = "2" + 10
         try:
             print(resultado)
         except TypeError:
             print("Error, no puedes sumar números y letras")
+        pass
 
-excepciones.comprobar() ; excepciones.comprobar2() ; excepciones.comprobar3() ; excepciones.comprobar4()
+excepciones.comprobar()
+excepciones.comprobar2()
+excepciones.comprobar3()
+excepciones.comprobar4()
