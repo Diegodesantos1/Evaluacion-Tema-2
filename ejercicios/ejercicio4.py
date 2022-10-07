8f53c46ab1c80e0e395d9aba11eb02a9e79fda77
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-07 16:04:28 +0200

    comprobación 2

--- ejercicios/ejercicio4.py
+++ ejercicios/ejercicio4.py
@@ -5,5 +5,12 @@ class excepciones():
             print(numero)
         except ZeroDivisionError:
             print("Error, no se puede dividir por 0")
+    def comprobar2():
+        lista = [4, 7, 30, 23, 5]
+        lista[10]
+        try:
+            print(lista[10])
+        except IndexError:
+            print("Error, no existe elemento con ese índice")
 
-excepciones.comprobar()
+excepciones.comprobar() ; excepciones.comprobar2()
