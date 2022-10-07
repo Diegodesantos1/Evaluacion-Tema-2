8af9cb5d8b83768721fb3086402f5fe4daa2557b
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-07 16:13:11 +0200

    reorganizado el código para elegir cual probar

--- ejercicios/ejercicio4.py
+++ ejercicios/ejercicio4.py
@@ -1,36 +1,35 @@
-class excepciones():
-    def comprobar():
-        numero = 7/0
-        try :
-            print(numero)
-        except ZeroDivisionError:
-            print("Error, no se puede dividir por 0")
-        pass
-    def comprobar2():
-        lista = [4, 7, 30, 23, 5]
-        lista[10]
-        try:
-            print(lista[10])
-        except IndexError:
-            print("Error, no existe elemento con ese índice")
-        pass
-    def comprobar3():
-        paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" }
-        paises["alemania"]
-        try:
-            print(paises["alemania"])
-        except KeyError:
-            print("Error, no existe ese elemento en el diccionario")
-        pass
-    def comprobar4():
-        resultado = "2" + 10
-        try:
-            print(resultado)
-        except TypeError:
-            print("Error, no puedes sumar números y letras")
-        pass
+class Excepciones():
+    def menu():
+        eleccion=int(input("¿Qué quieres comprobar?"))
+        if eleccion == 1:
+            numero = 7/0
+            try :
+                print(numero)
+            except ZeroDivisionError:
+                print("Error, no se puede dividir por 0")
+        elif eleccion == 2:
+            lista = [4, 7, 30, 23, 5]
+            lista[10]
+            try:
+                print(lista[10])
+            except IndexError:
+                print("Error, no existe elemento con ese índice")
+        elif eleccion == 3:
+            paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" }
+            paises["alemania"]
+            try:
+                print(paises["alemania"])
+            except KeyError:
+                print("Error, no existe ese elemento en el diccionario")
+        elif eleccion == 4:
+            resultado = "2" + 10
+            try:
+                print(resultado)
+            except TypeError:
+                print("Error, no puedes sumar números y letras")
+        else:
+            print("Error, no existe esa opción")
+            Excepciones.menu()
 
-excepciones.comprobar()
-excepciones.comprobar2()
-excepciones.comprobar3()
-excepciones.comprobar4()
+Excepciones.menu()
+
