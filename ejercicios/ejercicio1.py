1ab075d37f3a44cc916f6fccb48f252b3b5320b8
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-07 15:45:51 +0200

    creada la comprobación de la nota con un ejemplo

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -5,11 +5,18 @@ class Alumno:
         self.nota = nota
     def __str__(self):
         return f'El alumno se llama: {self.nombre} {self.apellido} y ha sacado un {self.nota}'
-    def crear_alumno():
+    def crear_alumno(self):
         nombre = str(input('Nombre: '))
         apellido = str(input('Apellido: '))
         nota = int(input('Nota: '))
         print("Se ha creado el alumno con éxito")
         print(Alumno(nombre, apellido, nota))
+    def calificacion(self):
+        if self.nota >= 5:
+            print('Aprobado')
+        else:
+            print('Suspenso')
 
-Alumno.crear_alumno()
+alumno = Alumno('Diego', 'de Santos', 7)
+alumno.calificacion()
+
