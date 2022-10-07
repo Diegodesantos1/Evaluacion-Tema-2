7aac6aceef332db3dc5d4ebb30280eaddbc3661a
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-10-07 15:35:36 +0200

    creada la función crear alumno y el método string

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -3,3 +3,12 @@ class Alumno:
         self.nombre = nombre
         self.apellido = apellido
         self.nota = nota
+    def __str__(self):
+        return f'El alumno se llama: {self.nombre} {self.apellido} y ha sacado un {self.nota}'
+    def crear_alumno():
+        nombre = str(input('Nombre: '))
+        apellido = str(input('Apellido: '))
+        nota = int(input('Nota: '))
+        print(Alumno(nombre, apellido, nota))
+
+Alumno.crear_alumno()
